47e34bd40a1fbc674badb3cbeac64e4b2708ea6f
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2022-06-11 20:11:43 +0530

    completed all things

--- components/order/Home.js
+++ components/order/Home.js
@@ -4,7 +4,7 @@ import OrderList from './OrderItem/orderList'
 export const Home = () => {
   return (
     <section>
-        <h1>Today Order </h1>
+        {/* <h1>Today Order </h1> */}
         <OrderList/>
     </section>
   )
--- components/order/OrderItem/orderList.js
+++ components/order/OrderItem/orderList.js
@@ -5,20 +5,29 @@ import React, { useMemo, useState, useEffect } from "react";
 const  OrderList = (props) => {
   //  console.log( "OrderList" , props );
     return (
-    <section>
-      {props.orders.map((order) => (
-        <OrderItem
-          key={order.id}
-          id={order.id}
-          cartItems={order.cartItems}
-          totalPrice= {order.totalPrice}
-          updatedAt= {order.updatedAt}
-        />
-      ))}
-       
+    <section style = {OrderListStyle}> 
+      {props.orders.length > 0 &&
+        <div>
+          {props.orders.map((order) => (
+            <OrderItem
+              key={order.id}
+              id={order.id}
+              cartItems={order.cartItems}
+              totalPrice= {order.totalPrice}
+              updatedAt= {order.updatedAt}
+            />
+          ))}  
+        </div>
+      }
+      {props.orders.length == 0 && <div> There is no Order Today go for hunt! </div> }
     </section>
     )
 }
 
 export default OrderList;
+
+const OrderListStyle = {
+  // border: '1px solid black',
+  marginTop: '10px',
+}
 
