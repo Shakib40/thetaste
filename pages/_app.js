a5e74bcbb5c31cdc4909e4bee5eca0bb61efe9d0
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2022-06-11 19:52:36 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout' 
-import { Provider } from "react-redux";
-import { wrapper } from "../redux/store";
+// import { Provider } from "react-redux";
+// import { wrapper } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,4 +12,4 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
