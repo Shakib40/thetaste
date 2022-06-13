9ef7b025445f5186aaed0d53b99b2b6f2fd45285
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2022-06-13 22:32:32 +0530

    Update Delivered.js

--- components/landing/Delivered/Delivered.js
+++ components/landing/Delivered/Delivered.js
@@ -13,7 +13,7 @@ export const Delivered = (props) => {
 
   const item =  props.delivered.filter( (data) => {
       //  return new Date( new Date().getTime() - ( 24 * 60 * 60 * 1000) )
-      
+      return 
   })
 
   console.log("Data" , item);
