974f73d50b0e8e8b8273f8c2b0e06b028a2ccd15
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2022-06-19 19:01:33 +0530

    gg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig
